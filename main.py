466a1ff4e6a361ca882f3d50b4b8e42bce4f055a
Shian Hui YAH <ysh20200130.kubs@gmail.com>
2023-01-20 01:02:20 +0800

    Draw shapes

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ tim = turtle.Turtle()
 my_screen = turtle.Screen()
 my_screen.colormode(255)
 
+
+def draw_shape(num_of_sides):
+    """Function to Draw Shape"""
+    for _ in range(1, num_of_sides+1):
+        tim.forward(100)
+        tim.right(360/num_of_sides)
+
 for x in range(3, 10):
     r = random.randint(1, 255)
     g = random.randint(1, 255)
     b = random.randint(1, 255)
     tim.pencolor(r, g, b)
-    for i in range(1, x+1):
-        tim.forward(100)
-        tim.right(360/x)
-        print(f"x is {x} and i is {i}")
-        print(360/x)
+    draw_shape(x)
+
 
 
 
