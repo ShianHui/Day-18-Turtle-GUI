4456f3a79e40b0c392132dc60a28bebdd2409b09
Shian Hui YAH <ysh20200130.kubs@gmail.com>
2023-01-20 00:45:34 +0800

    Draw shapes

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 """Main Module"""
 import turtle
-import heroes as h
-
+import random
 
 tim = turtle.Turtle()
 my_screen = turtle.Screen()
+my_screen.colormode(255)
 
-for i in range(1, 5):
-    tim.forward(100)
-    tim.right(90)
+for x in range(3, 10):
+    r = random.randint(1, 255)
+    g = random.randint(1, 255)
+    b = random.randint(1, 255)
+    tim.pencolor(r, g, b)
+    for i in range(1, x+1):
+        tim.forward(100)
+        tim.right(360/x)
+        print(f"x is {x} and i is {i}")
+        print(360/x)
 
-tim.pu()
-tim.left(90)
-tim.forward(20)
-tim.right(90)
 
-for i in range(1, 51):
-    if i % 2 == 0:
-        tim.pu()
-    else:
-        tim.pd()
-    tim.forward(10)
+
+
 
 my_screen.exitonclick()
-
-print(h.gen())
