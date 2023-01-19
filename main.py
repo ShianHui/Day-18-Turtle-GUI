219da3ff6eb70d8770b5bb37b643c9333ca79173
Shian Hui YAH <ysh20200130.kubs@gmail.com>
2023-01-20 01:10:40 +0800

    Draw shapes

--- main.py
+++ main.py
@@ -13,11 +13,16 @@ def draw_shape(num_of_sides):
         tim.forward(100)
         tim.right(360/num_of_sides)
 
+def randomize_color():
+    """Function to randomize Color"""
+    r_value = random.randint(1, 255)
+    g_value = random.randint(1, 255)
+    b_value = random.randint(1, 255)
+    tup_value = (r_value, g_value, b_value)
+    return tup_value
+
 for x in range(3, 10):
-    r = random.randint(1, 255)
-    g = random.randint(1, 255)
-    b = random.randint(1, 255)
-    tim.pencolor(r, g, b)
+    tim.pencolor(randomize_color())
     draw_shape(x)
 
 
